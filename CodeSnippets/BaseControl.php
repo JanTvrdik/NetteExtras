2d07d8cbc91b49a3c91da9ea9b57e61b4f440ea8
Jan Tvrdik <jan.tvrdik@gmail.com>
2010-12-06 16:20:01 +0100

    Added BaseControl

---
+++ CodeSnippets/BaseControl.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Addons and code snippets for Nette Framework. (unofficial)
+ *
+ * @author   Jan Tvrdík
+ * @license  MIT
+ */
+
+namespace JanTvrdik\CodeSnippets;
+
+use Nette;
+
+/**
+ * Base control for all controls in application.
+ *
+ * @author   Jan Tvrdík
+ */
+abstract class BaseControl extends Nette\Application\Control
+{
+	/** @var     bool              automatically derive template path from class name */
+	protected $autoSetupTemplateFile = TRUE;
+
+
+
+	/**
+	 * Automatically registers template file.
+	 *
+	 * @author   Jan Tvrdík
+	 * @return   Nette\Templates\FileTemplate
+	 */
+	protected function createTemplate()
+	{
+		$template = parent::createTemplate();
+		if ($this->autoSetupTemplateFile) $template->setFile($this->getTemplateFilePath());
+
+		return $template;
+	}
+
+
+
+	/**
+	 * Derives template path from class name.
+	 *
+	 * @author   Jan Tvrdík
+	 * @return   string
+	 */
+	protected function getTemplateFilePath()
+	{
+		$reflection = $this->getReflection();
+		$dir = dirname($reflection->getFileName());
+		$filename = $reflection->getShortName() . '.latte';
+		return $dir . \DIRECTORY_SEPARATOR . $filename;
+	}
+
+}

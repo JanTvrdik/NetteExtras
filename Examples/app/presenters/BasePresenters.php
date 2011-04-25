d41fc99269ce30daa5c393529403270ac27e1644
Jan Tvrdik <jan.tvrdik@gmail.com>
2011-04-25 08:20:44 +0200

    Examples: fixed FormMacros registration (temporary solution)

--- Examples/app/presenters/BasePresenters.php
+++ Examples/app/presenters/BasePresenters.php
@@ -18,14 +18,33 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 	/**
 	 * Setup templates filters.
 	 *
-	 * @author   Jan Tvrdík
+	 * @todo     Find a better way to register FormMacros!
+	 *
+	 * @author   Patrik Votoček, David Grudl
+	 * @license  CC BY-SA 3.0
 	 * @param    Nette\Templating\Template
 	 * @return   void
 	 */
 	public function templatePrepareFilters($template)
 	{
-		$latte = new Nette\Latte\Engine();
-		JanTvrdik\Templates\FormMacros::register($latte->getHandler());
-		$template->registerFilter($latte);
+		$template->registerFilter(function($s) {
+			$parser = new Nette\Latte\Parser;
+			$parser->setDelimiters('\\{(?![\\s\'"{}*])', '\\}');
+
+			// context-aware escaping
+			$parser->escape = '$template->escape';
+
+			// initialize handlers
+			$parser->handler = new Nette\Latte\DefaultMacros;
+			JanTvrdik\Templates\FormMacros::register($parser->handler);
+			$parser->handler->initialize($parser, $s);
+
+			// process all {tags} and <tags/>
+			$s = $parser->parse($s);
+
+			$parser->handler->finalize($s);
+
+			return $s;
+		});
 	}
 }

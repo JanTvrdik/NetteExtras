0025d0f0724a5bc364caa30ee421b8ba05dd3f25
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-06-23 10:23:32 +0200

    BaseControl: fixed compatibility with 2.0.x (released v0.3)

--- NetteExtras/CodeSnippets/BaseControl.php
+++ NetteExtras/CodeSnippets/BaseControl.php
@@ -14,7 +14,7 @@ use Nette;
  * Base control for all controls in application.
  *
  * @author   Jan Tvrdík
- * @version  0.2
+ * @version  0.3
  * @link     http://nette.merxes.cz/base-control/
  */
 abstract class BaseControl extends Nette\Application\UI\Control
@@ -28,11 +28,12 @@ abstract class BaseControl extends Nette\Application\UI\Control
 	 * Automatically registers template file.
 	 *
 	 * @author   Jan Tvrdík
+	 * @param    string
 	 * @return   Nette\Templating\FileTemplate
 	 */
-	protected function createTemplate()
+	protected function createTemplate($class = NULL)
 	{
-		$template = parent::createTemplate();
+		$template = parent::createTemplate($class);
 		if ($this->autoSetupTemplateFile) $template->setFile($this->getTemplateFilePath());
 
 		return $template;
